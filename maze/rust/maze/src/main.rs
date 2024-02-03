34306d722fe62e70e9d36dae99c930d8a5997fc4
Domrachev Alexandr <alexandr.domrachev@gmail.com>
2024-02-03 12:19:39 +0300

    refactor: maze crawler

--- maze/rust/maze/src/main.rs
+++ maze/rust/maze/src/main.rs
@@ -1,57 +1,17 @@
-use rand::seq::SliceRandom;
-use std::collections::{HashMap, HashSet};
-
+use maze::{Maze, MazeCell, MazeGenerator};
 use raylib::prelude::*;
+
+mod maze;
 
 const ROWS: usize = 40;
 const COLS: usize = 40;
 
-#[derive(Debug, Clone, Copy, PartialEq)]
-enum Cell {
-    Wall,
-    Path,
-}
-
-impl Default for Cell {
-    fn default() -> Self {
-        Cell::Wall
-    }
-}
-
-struct Maze {
-    cells: [[Cell; COLS]; ROWS],
-}
-
-impl Maze {
-    fn fill_maze_with(&mut self) {
-        for i in 0..ROWS {
-            for j in 0..COLS {
-                if (i + j) % 2 == 0 {
-                    self.cells[i][j] = Cell::Path;
-                }
-            }
-        }
-    }
-}
-
-impl Default for Maze {
-    fn default() -> Self {
-        Maze {
-            cells: [[Cell::default(); COLS]; ROWS],
-        }
-    }
-}
-
 fn main() {
     let (mut rl, thread) = raylib::init().size(800, 800).title("Maze crawler").build();
 
-    let mut maze = Maze::default();
-    maze.fill_maze_with();
-    for _ in 0..20 {
-        generate_maze(&mut maze);
-    }
-
-    print_maze(&maze);
+    let mut maze = Maze::new(ROWS, COLS);
+    maze.generate_maze(1, 1);
+    println!("{:?}", maze);
 
     while !rl.window_should_close() {
         let mut d = rl.begin_drawing(&thread);
@@ -61,98 +21,18 @@ fn main() {
     }
 }
 
-fn generate_maze(maze: &mut Maze) {
-    let mut sets: HashMap<usize, HashSet<usize>> = HashMap::new();
-    for row in 0..ROWS {
-        for col in 0..COLS {
-            if !sets.contains_key(&col) {
-                sets.insert(col, HashSet::new());
-            }
-            sets.get_mut(&col).unwrap().insert(row);
-            if col > 0 && should_carve(&sets, col) {
-                carve_horizontaly(&mut sets, col, row, maze);
-            }
-            if row > 0 && should_carve(&sets, col) {
-                carve_vertically(&mut sets, col, row, maze);
-            }
-        }
-        sets.retain(|_, v| !v.is_empty());
-    }
-}
-
-fn should_carve(sets: &HashMap<usize, HashSet<usize>>, col: usize) -> bool {
-    if let Some(current_set) = sets.get(&col) {
-        if let Some(next_set) = sets.get(&(col + 1)) {
-            return !current_set.is_disjoint(next_set);
-        }
-    }
-    false
-}
-
-fn carve_horizontaly(
-    sets: &mut HashMap<usize, HashSet<usize>>,
-    col: usize,
-    row: usize,
-    maze: &mut Maze,
-) {
-    let current_set = sets.get(&col).unwrap().clone();
-    let next_set = sets.get(&(col + 1)).unwrap().clone();
-    let mut intersection = Vec::new();
-    for &element in &current_set {
-        if next_set.contains(&element) {
-            intersection.push(element);
-        }
-    }
-    let mut rng = rand::thread_rng();
-    let remove_wall = *intersection.choose(&mut rng).unwrap_or(&0);
-    maze.cells[remove_wall][col] = Cell::Path;
-    sets.get_mut(&col).unwrap().insert(row);
-    sets.get_mut(&(col + 1)).unwrap().insert(row);
-    sets.get_mut(&col).unwrap().extend(next_set);
-    sets.remove(&(col + 1));
-}
-
-fn carve_vertically(
-    sets: &mut HashMap<usize, HashSet<usize>>,
-    col: usize,
-    row: usize,
-    maze: &mut Maze,
-) {
-    let current_set = sets.get(&col).unwrap().clone();
-    let next_set = sets.get(&(col + 1)).unwrap().clone();
-    let available_rows: Vec<_> = current_set.iter().cloned().collect();
-    let mut rng = rand::thread_rng();
-    let remove_wall = available_rows.choose(&mut rng).unwrap();
-    maze.cells[row][*remove_wall] = Cell::Path;
-    sets.get_mut(&col).unwrap().insert(row);
-    sets.get_mut(&col).unwrap().extend(next_set.clone());
-    sets.get_mut(&(col + 1)).unwrap().extend(next_set);
-    sets.remove(&(col + 1));
-}
-
-fn print_maze(maze: &Maze) {
-    for row in maze.cells.iter() {
-        for &cell in row.iter() {
-            match cell {
-                Cell::Wall => print!("■ "),
-                Cell::Path => print!("1 "),
-            }
-        }
-        println!();
-    }
-}
-
 fn draw_maze(maze: &Maze, d: &mut RaylibDrawHandle) {
     let cell_size: i32 = 16;
-    for (i, row) in maze.cells.iter().enumerate() {
-        for (j, &cell) in row.iter().enumerate() {
-            let color = match cell {
-                Cell::Wall => Color::LIGHTGRAY,
-                Cell::Path => Color::LIME,
+    for y in 0..maze.height {
+        for x in 0..maze.width {
+            let color = match maze.get_cell(x, y) {
+                MazeCell::Wall => Color::LIGHTGRAY,
+                MazeCell::Path => Color::BLACK,
+                _ => Color::BLACK,
             };
             d.draw_rectangle(
-                i as i32 * cell_size,
-                j as i32 * cell_size,
+                x as i32 * cell_size,
+                y as i32 * cell_size,
                 cell_size,
                 cell_size,
                 color,
---
+++ maze/rust/maze/src/maze.rs
@@ -0,0 +1,120 @@
+use rand::rngs::ThreadRng;
+use rand::seq::SliceRandom;
+use std::fmt;
+
+#[derive(Clone, Copy, PartialEq)]
+pub enum MazeCell {
+    Wall,
+    Path,
+    Entrance,
+    Exit,
+}
+
+pub trait MazeGenerator {
+    fn generate_maze(&mut self, start_x: usize, start_y: usize);
+    fn depth_first_search(
+        &mut self,
+        x: i32,
+        y: i32,
+        rng: &mut ThreadRng,
+        directions: &[(i32, i32)],
+    );
+}
+
+pub struct Maze {
+    pub width: usize,
+    pub height: usize,
+    cells: Vec<MazeCell>,
+}
+
+impl Maze {
+    pub fn new(width: usize, height: usize) -> Self {
+        let cells = vec![MazeCell::Wall; width * height];
+        Maze {
+            width,
+            height,
+            cells,
+        }
+    }
+
+    pub fn get_index(&self, x: usize, y: usize) -> usize {
+        y * self.width + x
+    }
+
+    pub fn get_cell(&self, x: usize, y: usize) -> MazeCell {
+        self.cells[self.get_index(x, y)]
+    }
+
+    pub fn set_cell(&mut self, x: usize, y: usize, value: MazeCell) {
+        let index = self.get_index(x, y);
+        self.cells[index] = value;
+    }
+
+    pub fn is_valid_move(&self, x: i32, y: i32) -> bool {
+        x >= 0 && y >= 0 && x < self.width as i32 && y < self.height as i32
+    }
+}
+
+impl MazeGenerator for Maze {
+    fn generate_maze(&mut self, start_x: usize, start_y: usize) {
+        let mut rng = rand::thread_rng();
+        let directions = [(0, -2), (0, 2), (-2, 0), (2, 0)];
+
+        self.set_cell(start_x, start_y, MazeCell::Path);
+        MazeGenerator::depth_first_search(
+            self,
+            start_x as i32,
+            start_y as i32,
+            &mut rng,
+            &directions,
+        )
+    }
+
+    fn depth_first_search(
+        &mut self,
+        x: i32,
+        y: i32,
+        rng: &mut ThreadRng,
+        directions: &[(i32, i32)],
+    ) {
+        let mut shuffled_directions = directions.to_vec();
+        shuffled_directions.shuffle(rng);
+        for &(dx, dy) in &shuffled_directions {
+            let new_x = x + dx;
+            let new_y = y + dy;
+
+            if self.is_valid_move(new_x, new_y) {
+                let nx = new_x as usize;
+                let ny = new_y as usize;
+
+                if self.get_cell(nx, ny) == MazeCell::Wall {
+                    self.set_cell(nx, ny, MazeCell::Path);
+                    self.set_cell(
+                        (x + new_x) as usize / 2,
+                        (y + new_y) as usize / 2,
+                        MazeCell::Path,
+                    );
+
+                    self.depth_first_search(new_x, new_y, rng, directions);
+                }
+            }
+        }
+    }
+}
+
+impl fmt::Debug for Maze {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for y in 0..self.height {
+            for x in 0..self.width {
+                match self.get_cell(x, y) {
+                    MazeCell::Wall => write!(f, "#")?,
+                    MazeCell::Path => write!(f, " ")?,
+                    MazeCell::Entrance => write!(f, ">")?,
+                    MazeCell::Exit => write!(f, "E")?,
+                }
+            }
+            writeln!(f)?;
+        }
+        Ok(())
+    }
+}
