0f39ddc620d49e14d2a9597e4f2d88383ccb018e
Domrachev Alexandr <alexandr.domrachev@gmail.com>
2024-02-13 09:56:56 +0300

    update: Maze crawler: draw AStar visualization

--- maze/rust/maze/src/main.rs
+++ maze/rust/maze/src/main.rs
@@ -1,3 +1,7 @@
+use std::thread;
+use std::time::Duration;
+
+use astar::AStar;
 use pathfinding::PathfindingAlgorithm;
 
 use crate::backtracking::Backtracking;
@@ -15,9 +19,17 @@ const COLS: usize = 41;
 fn main() {
     let (mut rl, thread) = raylib::init().size(800, 800).title("Maze crawler").build();
 
+    // Backtracking algorithm
     let mut maze_visualization = MazeVisualization::new(ROWS, COLS, &mut rl, &thread);
     let mut backtracking_algorithm = Backtracking::new(true, 20);
     if backtracking_algorithm.find_path(&mut maze_visualization) {}
 
-    maze_visualization.visualize(backtracking_algorithm.name());
+    thread::sleep(Duration::from_secs(2));
+
+    // AStar algorithm
+    let mut maze_visualization = MazeVisualization::new(ROWS, COLS, &mut rl, &thread);
+    let mut astar_algorithm = AStar::new(20);
+    if astar_algorithm.find_path(&mut maze_visualization) {}
+
+    maze_visualization.visualize(astar_algorithm.name());
 }
